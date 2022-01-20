8ccc3ea5185342afee20d5f0cf62646482bfc4e9
Your Name <you@example.com>
2022-01-20 18:26:55 +0700

    second

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name = "Shop";
+    $host = "us-cdbr-east-05.cleardb.net";
+    $username = "b628b94fa3a955";
+    $password = "cd625c80";
+    $db_name = "heroku_edaba2f5ebed181";
 
     $con = mysqli_connect($host, $username, $password, $db_name); //เชื่อมต่อกับฐานข้อมูล
 
